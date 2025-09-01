9e69e1855051d8b9e7e0987567aa86c2175975fd
Vinod Kumar <vinodkumar@192.168.1.2>
2025-09-01 19:28:51 +0530

    Initial Commit

---
+++ src/main/java/com/hibernate_spring_boot_utc_timezone/HibernateSpringBootUtcTimezoneApplication.java
@@ -0,0 +1,13 @@
+package com.hibernate_spring_boot_utc_timezone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateSpringBootUtcTimezoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HibernateSpringBootUtcTimezoneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hibernate_spring_boot_utc_timezone/HibernateSpringBootUtcTimezoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hibernate_spring_boot_utc_timezone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HibernateSpringBootUtcTimezoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
