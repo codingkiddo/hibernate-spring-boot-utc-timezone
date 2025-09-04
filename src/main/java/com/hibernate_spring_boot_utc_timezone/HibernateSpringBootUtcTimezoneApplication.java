2885d4174bdd84d1b6aa898a21ba8428452e959c
Vinod Kumar <vinodkumar@192.168.1.2>
2025-09-04 09:37:27 +0530

    modified:   src/main/java/com/hibernate_spring_boot_utc_timezone/HibernateSpringBootUtcTimezoneApplication.java 	new file:   src/main/java/com/hibernate_spring_boot_utc_timezone/service/ScreenshotService.java

--- src/main/java/com/hibernate_spring_boot_utc_timezone/HibernateSpringBootUtcTimezoneApplication.java
+++ src/main/java/com/hibernate_spring_boot_utc_timezone/HibernateSpringBootUtcTimezoneApplication.java
@@ -1,13 +1,31 @@
 package com.hibernate_spring_boot_utc_timezone;
 
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.hibernate_spring_boot_utc_timezone.service.ScreenshotService;
 
 @SpringBootApplication
 public class HibernateSpringBootUtcTimezoneApplication {
 
+	private final ScreenshotService screenshotService;
+	
+	public HibernateSpringBootUtcTimezoneApplication(ScreenshotService screenshotService) {
+        this.screenshotService = screenshotService;
+    }
+	
 	public static void main(String[] args) {
 		SpringApplication.run(HibernateSpringBootUtcTimezoneApplication.class, args);
 	}
+	
+	@Bean
+    public ApplicationRunner init() {
+        return args -> {
+            screenshotService.saveScreenshotInUTC();
+            screenshotService.displayScreenshotInUTC();
+        };
+    }
 
 }
